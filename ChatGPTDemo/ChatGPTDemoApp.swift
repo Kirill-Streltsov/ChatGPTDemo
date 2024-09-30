cd8f16030c7ff78d06284cad279486521558e36b
Kirill-Streltsov <krlsrlsv@gmail.com>
2024-09-30 16:03:57 +0200

    Initial Commit

---
+++ ChatGPTDemo/ChatGPTDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTDemoApp.swift
+//  ChatGPTDemo
+//
+//  Created by Kirill Streltsov on 30.09.24.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatGPTDemo
+//
+//  Created by Kirill Streltsov on 30.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
